b57b4534184dabae4935c49e596053caee0abf19
Arielle <arielle@viam.com>
2023-11-02 19:58:31 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
